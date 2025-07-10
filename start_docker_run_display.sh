b795de416309bf5a11160aaf679e7ddae20e6f07
xuzhihaouuu <91545024+xuzhihaoup@users.noreply.github.com>
2025-07-10 17:32:25 +0800

    Add files via upload

---
+++ start_docker_run_display.sh
@@ -0,0 +1,21 @@
+#配置容器启动并映射图像界面
+xhost local:root
+
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+#如果你想执行后删除容器加上--rm
+docker run -it \
+    --gpus all \
+    --name isaac_sim_container \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --volume ${HOME}/.Xauthority:/root/.Xauthority:rw \
+    --env XAUTHORITY=$XAUTH \
+    --volume $XAUTH:$XAUTH \
+    --net host \
+    --privileged \
+    -v $(pwd):/workspace \
+    crpi-p4y4hs8uwp1hkhn1.cn-shanghai.personal.cr.aliyuncs.com/isaac_sim/isaac_sim_syjs:isaacsim_4.1.0_py310_ubuntu22.04_torch2.5.1_cuda122 \
+    bash
